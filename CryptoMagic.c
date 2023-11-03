e3e37b95cc32d3e3c50e479b1238f909a467d9eb
DenisSamuel <samueld@uoguelph.ca>
2023-11-02 22:34:51 -0400

    added file opener and closer for encryption and decryption code

--- CryptoMagic.c
+++ CryptoMagic.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-void Encrypt(const char *filename){ //Encryption Function
+//Encryption Function
 
+void Encrypt(const char *filename){ 
+    FILE *inputfile = fopen(filename, "r"); // opens file specified by filename in read mode
+    FILE *outputfile = fopen("myFile.txt", "w"); //opens file named "text.crp" in write mode
+
+    int c;
+    while((c = fgetc(inputfile)) != EOF){ //reads each character in file until the end of the file is reached
+        //encryption code here
+    }
+    fclose(inputfile); //closes files
+    fclose(outputfile);
 }
 
-void Decrypt(const char *filename){ //Decryption function
+//Decryption function
 
+void Decrypt(const char *filename){ 
+    FILE *inputfile = fopen(filename, "r"); // opens file specified by filename in read mode
+    FILE *outputfile = fopen("myFile.crp", "w"); //opens file named "text.crp" in write mode
+
+    int c;
+    while((c = fgetc(inputfile)) != EOF){ //reads each character in file until the end of the file is reached
+        //decryption code here
+    }
+    fclose(inputfile); //closes files
+    fclose(outputfile);
 }
 
-int main(int argc, char *argv[]){ //number of arguments passed, array containing arguments
+//main function
+
+int CryptoMagic(int argc, char *argv[]){ //number of arguments passed, array containing arguments
     if(argc == 3 && strcmp(argv[1], "CryptoMagic") == 0){ //checks if the argument is 3 and if the first is "CryptoMagic"
         const char *filename = argv[2]; //extracts the filename and stores it in the filename variable
         if (strstr(filename, ".crp") != NULL) { //checks for ".crp" and if the extension is in the file name and then it calls the decrypt function
-            decryptFile(filename); //decyrpt function
+            Decrypt(filename); //decyrpt function
         } else {
-            encryptFile(filename); //encrypt function
+            Encrypt(filename); //encrypt function
         }
     }
+     return 0;
     }
     
 
-    return 0;
-}
+    
